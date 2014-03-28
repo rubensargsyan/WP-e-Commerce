aeb90f5b64a94f6a0a8f51a4f19beac4214ea3f3
Ruben Sargsyan <ruben_sargsyan@yahoo.com>
2014-03-28 01:31:32 -0700

    Product image notice is fixed.
    Fix for #983

--- wpsc-components/theme-engine-v1/helpers/template-tags.php
+++ wpsc-components/theme-engine-v1/helpers/template-tags.php
@@ -544,7 +544,7 @@ function wpsc_product_image( $attachment_id = 0, $width = null, $height = null )
 			$image_meta[$meta_name] = maybe_unserialize( array_pop( $meta_value ) );
 
 
-		$attachment_metadata = $image_meta['_wp_attachment_metadata'];
+		$attachment_metadata = isset( $image_meta['_wp_attachment_metadata'] ) ? $image_meta['_wp_attachment_metadata'] : '';
 		// Determine if we already have an image of this size
 		if ( isset( $attachment_metadata['sizes'] ) && (count( $attachment_metadata['sizes'] ) > 0) && ( isset( $attachment_metadata['sizes'][$intermediate_size] ) ) ) {
 			$intermediate_image_data = image_get_intermediate_size( $attachment_id, $intermediate_size );
